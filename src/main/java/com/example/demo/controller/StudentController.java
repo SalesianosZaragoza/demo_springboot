2e634e835f7e9506eda5ba2917f272d375a38a6f
Gorka Sanz <gorka.sanz@salesianos.edu>
2023-10-25 09:44:25 +0200

    inestable

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,13 +1,18 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.controller.database.H2Connection;
 import com.example.demo.model.Student;
 
 @Controller
 public class StudentController {
+	@Autowired
+	H2Connection h2;
+
 	// localhost:8080/insertStudent
 	@RequestMapping("/insertStudent")
 	public String insertarEstudiante(Student student, Model model) {
---
+++ src/main/java/com/example/demo/controller/database/H2Connection.java
@@ -0,0 +1,8 @@
+package com.example.demo.controller.database;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class H2Connection {
+
+}
