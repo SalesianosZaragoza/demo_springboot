40edf26992d1d71ab476bb328431caf0e66694c5
Gorka Sanz <gorka.sanz@salesianos.edu>
2023-11-08 09:10:23 +0100

    re added getter and setter

--- src/main/java/com/example/demo/model/Student.java
+++ src/main/java/com/example/demo/model/Student.java
@@ -9,5 +9,23 @@ public class Student {
 	private Integer id;
 	private String nombre;
 	private String apellido;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
 
 }
