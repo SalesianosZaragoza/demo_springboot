c473a317e960b0048cfd8e854e86f46e42548ca0
Alberto Saz <albertosaz@gmail.com>
2023-11-15 14:31:59 +0100

    more clean

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -3,26 +3,18 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.example.demo.controller.database.DBConnection;
 import com.example.demo.model.Student;
 import com.example.demo.service.StudentService;
 
 @Controller
 public class StudentController {
-	@Autowired // usa esta interfaz
-	// @Qualifier(value = "postgres") // me conectas el que tenga este valor
-	// automáticamente
-	DBConnection db;
 
-	@Autowired
-	JdbcTemplate jdbcTemplate;
 
 	@Autowired
 	StudentService service;
